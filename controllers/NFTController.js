f61924aa052e54715a139b79b59415374e1e85d8
Sumit Paul <sumit.18.paul@gmail.com>
2023-02-25 23:35:39 +0530

    puppetter test

--- controllers/NFTController.js
+++ controllers/NFTController.js
@@ -78,7 +78,7 @@ exports.screenshot = [
   async (req, res) => {
     const url = req.body.url
     try {
-      let browser = await chromium.puppeteer.launch({
+      let browser = chromium.puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
         executablePath: await chromium.executablePath,
@@ -87,7 +87,7 @@ exports.screenshot = [
       });
       console.log("here");
       //let page = await browser.newPage();
-      page.goto(url);
+      //page.goto(url);
       console.log("here2");
       //const screenshot = page.screenshot({ encoding: 'base64' });
       return apiResponse.successResponseWithData(res, "Here: ", "screenshot")
